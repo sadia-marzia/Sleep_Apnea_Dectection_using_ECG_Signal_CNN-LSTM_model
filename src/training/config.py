049145c7c2dab395b06171c90ce677888a779284
Sadiatul Marzia <133556752+sadia-marzia@users.noreply.github.com>
2026-01-08 01:23:23 +0600

    Modify training configuration settings
    Updated model path and configuration parameters.

--- src/training/config.py
+++ src/training/config.py
@@ -1,14 +1,11 @@
-# src/training/config.py
-
 SEED = 42
 
 BATCH_SIZE = 32
 EPOCHS = 40
 LEARNING_RATE = 0.0005
 
-TEST_SIZE = 0.2
-N_FOLDS = 10
-
+N_SPLITS = 10
 DROPOUT_RATE = 0.25
+L2_REG = 0.0005
 
-MODEL_PATH = "artifacts/cnn_lstm_model.keras"
+MODEL_PATH = "artifacts/cnn_lstm_apneamodel.keras"
