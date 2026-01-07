a3b8438bfd1678b9281c17a6db66492a2b59fb2b
Sadiatul Marzia <133556752+sadia-marzia@users.noreply.github.com>
2026-01-08 01:21:25 +0600

    Create config.py

---
+++ src/training/config.py
@@ -0,0 +1,14 @@
+# src/training/config.py
+
+SEED = 42
+
+BATCH_SIZE = 32
+EPOCHS = 40
+LEARNING_RATE = 0.0005
+
+TEST_SIZE = 0.2
+N_FOLDS = 10
+
+DROPOUT_RATE = 0.25
+
+MODEL_PATH = "artifacts/cnn_lstm_model.keras"
